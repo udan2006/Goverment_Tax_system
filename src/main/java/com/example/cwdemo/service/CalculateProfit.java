7aae17c9ad01e32b888abf079fb30ee151cc2a08
udan2006 <udan.malewana@gmail.com>
2025-04-28 00:03:19 +0530

    edit profit calculation

--- src/main/java/com/example/cwdemo/service/CalculateProfit.java
+++ src/main/java/com/example/cwdemo/service/CalculateProfit.java
@@ -5,7 +5,7 @@ import com.example.cwdemo.model.Transaction;
 // this method used to calculate the profit for a given transaction
 public class CalculateProfit {
     public double calculate(Transaction transaction) {
-        return ((transaction.getSalesPrice() * transaction.getQuantity()) - transaction.getDiscount()) -
+        return (transaction.getSalesPrice() * transaction.getQuantity() - transaction.getDiscount()) -
                 (transaction.getInternalPrice() * transaction.getQuantity()) ;
     }
 }
